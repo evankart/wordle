ffdda41ef1139dd14be388b080f371833f54c1e9
Evan Kartheiser <ekarthei@gmail.com>
2022-08-31 14:27:30 -0700

    added functionality to keyboard, started assigning colors to correct guesses. Updated styling to match nytime wordle

--- javascript.js
+++ javascript.js
@@ -1,48 +1,149 @@
-const hiddenWord = "abriq"; // The word you're trying to guess
+const wordBank = [
+  "Amber",
+  "Ashen",
+  "Azure",
+  "Beige",
+  "Beryl",
+  "Black",
+  "Blond",
+  "Blush",
+  "Brown",
+  "Coral",
+  "Cream",
+  "Dusky",
+  "Ebony",
+  "Eosin",
+  "Flame",
+  "Green",
+  "Gules",
+  "Hazel",
+  "Henna",
+  "Hoary",
+  "Indol",
+  "Ivory",
+  "Khaki",
+  "Lemon",
+  "Liard",
+  "Liart",
+  "Lilac",
+  "Livid",
+  "Lovat",
+  "Lyart",
+  "Mauve",
+  "Milky",
+  "Mocha",
+  "Mousy",
+  "Murex",
+  "Ochre",
+  "Olive",
+  "Orcin",
+  "Orpin",
+  "Pansy",
+  "Peach",
+  "Pearl",
+  "Rouge",
+  "Ruddy",
+  "Sable",
+  "Sandy",
+  "Sepia",
+  "Smoky",
+  "Snowy",
+  "Sooty",
+  "Steel",
+  "Straw",
+  "Swart",
+  "Taupe",
+  "Tawny",
+  "Topaz",
+  "Umber",
+  "Virid",
+  "Wheat",
+  "White",
+];
 
-let guessedWord = "abri"; // Starting guess ("")
+let hiddenWord = wordBank[0].toLowerCase(); // The word you're trying to guess
 
-const box1 = document.querySelector(".box"); // first box in first row
+let guessedWord = ""; // Starting guess ("")
 
-const q = document.querySelector(".key"); // 'q' key event listener
+let guesses = 0;
 
-const erase = document.querySelector(".erase"); // delete key event listener
+const deleteKey = document.querySelector(".deleteKey"); // deleteKey key event listener
 
 const enter = document.querySelector(".enter"); // enter key event listener
 
-// TO DO: Add event listeners to all keyboard keys
-
-// TO DO: Find the next open box
-
-// When a key is pressed, add that letter to the next open box and add the letter to the guessed word
-q.addEventListener("click", function () {
-  $(".box:empty:first").text("Q"); // finds the first empty box and changes the text to 'q'
-  guessedWord = guessedWord.concat("q"); // adds 'q' to the end of current guess
-  console.log(guessedWord);
+// Add event listeners to all keyboard keys, add selected letter to guess
+document.querySelectorAll(".key").forEach((item) => {
+  item.addEventListener("click", (event) => {
+    if (guessedWord.length < 5) {
+      // only allow adding letters if the word is less than 5 letters long
+      targetBox = document.querySelector(".box:empty"); // sets the target to the next open box
+      targetBox.innerHTML = item.innerHTML; // places selected letter in the target box
+      guessedWord = guessedWord.concat(item.innerHTML.toLowerCase()); // adds letter to the end of current guess
+      console.log(guessedWord);
+    }
+  });
 });
+
+// FUNCTION: Clear the board and reset the game, then pick a new hidden word
+function resetGame() {
+  let boxes = document.querySelectorAll(".box");
+  boxes.forEach((item) => {
+    item.innerHTML = ""; // clears the previous guesses from the board
+    item.classList.remove("green", "yellow"); // removes background colors form boxes on the board
+  });
+  guessedWord = ""; // resets the current guess to ""
+  hiddenWord = wordBank[1]; // changes the hidden word to a new word
+  guesses = 0; // resets the number of guesses to zero for new round
+}
 
 // FUNCTION: Check if the word is correct when clicking enter
 enter.addEventListener("click", function () {
-  if (guessedWord.toLowerCase() === hiddenWord.toLowerCase()) {
-    console.log("you guessed it!");
+  if (guessedWord.length == 5) {
+    // Changes the correctly guessed letters to green
+    let boxes = document.querySelectorAll(".box");
+    let j = 0;
+    boxes.forEach((item) => {
+      if (item.innerHTML != "") {
+        if (item.innerHTML.toLowerCase() == hiddenWord[j]) {
+          item.classList.add("green");
+        } else if (
+          /* if letter is in the word in another place in the word*/
+          hiddenWord.includes(item.innerHTML.toLowerCase())
+        ) {
+          item.classList.add("yellow");
+          console.log(j + " no");
+        }
+      }
+      j++;
+    });
+
+    // TO DO: Bring color changes to each new row as you play
+
+    // Checks that guess is 5 letters long
+    if (guessedWord.toLowerCase() === hiddenWord.toLowerCase()) {
+      // Check if the guessed word is correct
+      console.log("you guessed it!");
+      resetGame();
+    } else {
+      console.log("Not this time!");
+    }
+    guessedWord = "";
+    guesses++;
+    console.log("Guess Number: " + guesses);
   } else {
-    console.log("Not this time!");
+    console.log("Please enter a five letter word."); // If guess is not 5 letters long, promts the user
+  }
+
+  if (guesses >= 6) {
+    alert("Better luck next time!");
+    resetGame();
   }
 });
 
-// FUNCTION: Check if the guessed word is correct
-function checkWord() {}
-
-checkWord();
-
-// TO DO: After a letter has been added, update the next open box
-
-// FUNCTION: Erase the last letter from your guess
-erase.addEventListener("click", function () {
-  box1.innerHTML = ""; // empties square one (temp)
+// FUNCTION: deleteKey the last letter from your guess
+deleteKey.addEventListener("click", function () {
+  targetBox.innerHTML = ""; // deleteKeys the last letter added
+  targetBox = targetBox.previousElementSibling; // changes the target box to previous letter added
   guessedWord = guessedWord.slice(0, -1); // keeps all but the last letter of your guess
   console.log(guessedWord);
-  lastBox = document.querySelector(".box:not(:empty)");
 });
-
-// TO DO: change the text of the last filled in square to "" when clicking 'delete'
