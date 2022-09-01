15cdfc2df138b52f791fc3e26e195d0d06d430d1
Evan Kartheiser <ekarthei@gmail.com>
2022-09-01 14:21:17 -0700

    cleaned up function formatting

--- javascript.js
+++ javascript.js
@@ -47,26 +47,30 @@ const wordBank = [
   "White",
 ];
 
-// TO DO: DOn't show duplicate yellow sqares if letter is only in the word once
+// OUTLINE
+
+// 1. DEFINE VARIABLES
 
 randomIndex = Math.floor(Math.random() * wordBank.length);
 
-let hiddenWord = wordBank[randomIndex].toLowerCase(); // The word you're trying to guess
+let hiddenWord = "abbot"; // wordBank[randomIndex].toLowerCase() for random word
 
-let guessedWord = ""; // Starting guess ("")
+let guessedWord = ""; // Blank starting guess
 
-let guesses = 0;
+let guesses = 0; // Staerting at guess number 0
 
-const deleteKey = document.querySelector(".deleteKey"); // deleteKey key event listener
+const boxes = document.querySelectorAll(".box"); // boxes = all of the boxes on the board
 
-const enter = document.querySelector(".enter"); // enter key event listener
+const letterKeys = document.querySelectorAll(".letterKey"); // keys = all of the keyboard keys
 
-const boxes = document.querySelectorAll(".box");
+const deleteKey = document.querySelector(".deleteKey"); // deleteKey
 
-// Add event listeners to all keyboard keys, add selected letter to guess
-const keys = document.querySelectorAll(".key");
+const enterKey = document.querySelector(".enterKey"); // enter key
 
-keys.forEach((item) => {
+// 2. ADD EVENT LISTENERS
+
+// When a letter is pressed (and current guess is <5 chars) find the next open box, add the letter to it, darken the key bg color, and add the letter to the current guess.
+letterKeys.forEach((item) => {
   item.addEventListener("click", (event) => {
     if (guessedWord.length < 5) {
       // only allow adding letters if the word is less than 5 letters long
@@ -79,61 +83,28 @@ keys.forEach((item) => {
   });
 });
 
-// FUNCTION: Clear the board and reset the game, then pick a new hidden word
-function resetGame() {
-  let boxes = document.querySelectorAll(".box");
-  boxes.forEach((item) => {
-    item.innerHTML = ""; // clears the previous guesses from the board
-    item.classList.remove("green", "yellow"); // removes background colors form boxes on the board
-  });
-  guessedWord = ""; // resets the current guess to ""
-  hiddenWord = wordBank[randomIndex]; // changes the hidden word to a new word
-  guesses = 0; // resets the number of guesses to zero for new round
-  keys.forEach((item) => {
-    item.classList.remove("dark"); // resets keyboard background color
-  });
-}
+// When enter key is pressed - run the checkGuess function
+enterKey.addEventListener("click", checkGuess);
+
+// When delete key is pressed - run the deleteLetter function
+deleteKey.addEventListener("click", deleteLetter);
+
+// 3. DEFINE FUNCTIONS
 
 // FUNCTION: Check if the word is correct when clicking enter
-let j = 0;
-enter.addEventListener("click", checkGuess);
-
 function checkGuess() {
   if (guessedWord.length == 5) {
     // Checks that guess is 5 letters long
-
     if (guessedWord.toLowerCase() === hiddenWord.toLowerCase()) {
       // Check if the guessed word is correct
-      boxes.forEach((item) => {
-        item.classList.add("green");
-      });
-      console.log("you guessed it!");
-      alert("Congrats! You did it!");
-      resetGame();
+      youWin();
     } else {
       console.log("Not this time!");
     }
     guessedWord = "";
     guesses++;
 
-    // Changes the correctly guessed letters to green
-    boxes.forEach((item) => {
-      if (item.innerHTML != "") {
-        if (item.innerHTML.toLowerCase() == hiddenWord[j]) {
-          item.classList.add("green");
-        } else if (
-          /* if letter is in the word in another place in the word*/
-          hiddenWord.includes(item.innerHTML.toLowerCase())
-        ) {
-          item.classList.add("yellow");
-        }
-      }
-      j++;
-      if (j == 5) {
-        // restets j count to 0 once it reaches 5
-        j = 0;
-      }
-    });
+    addColors();
   } else {
     console.log("Please enter a five letter word."); // If guess is not 5 letters long, promts the user
   }
@@ -144,10 +115,59 @@ function checkGuess() {
   }
 }
 
+//FUNCTION: Change relevant squares to green or yellow
+function addColors() {
+  let j = 0;
+  boxes.forEach((item) => {
+    if (item.innerHTML != "") {
+      if (item.innerHTML.toLowerCase() == hiddenWord[j]) {
+        item.classList.add("green");
+      } else if (hiddenWord.includes(item.innerHTML.toLowerCase())) {
+        item.classList.add("yellow");
+
+        let numInstances =
+          hiddenWord.split(item.innerHTML.toLowerCase()).length - 1;
+        console.log(item.innerHTML + " appears " + numInstances + " times.");
+      }
+    }
+    j++;
+    // resets j count to 0 once it reaches 5
+    if (j == 5) {
+      j = 0;
+    }
+  });
+}
+
+// FUNCTION: Make all squares green and call resetGame once you've guessed correctly
+function youWin() {
+  boxes.forEach((item) => {
+    item.classList.add("green");
+  });
+  alert("Congrats! You did it!");
+  resetGame();
+}
+
+// FUNCTION: Clear the board and reset the game, then pick a new hidden word
+function resetGame() {
+  let boxes = document.querySelectorAll(".box");
+  boxes.forEach((item) => {
+    item.innerHTML = ""; // clears the previous guesses from the board
+    item.classList.remove("green", "yellow"); // removes background colors form boxes on the board
+  });
+  guessedWord = ""; // resets the current guess to ""
+  hiddenWord = wordBank[randomIndex].toLowerCase(); // changes the hidden word to a new word
+  guesses = 0; // resets the number of guesses to zero for new round
+  letterKeys.forEach((item) => {
+    item.classList.remove("dark"); // resets keyboard background color
+  });
+}
+
 // FUNCTION: delete the last letter from your guess
-deleteKey.addEventListener("click", function () {
+function deleteLetter() {
   targetBox.innerHTML = ""; // delete the last letter added
   targetBox = targetBox.previousElementSibling; // changes the target box to previous letter added
   guessedWord = guessedWord.slice(0, -1); // keeps all but the last letter of your guess
   // console.log(guessedWord);
-});
+}
+
+// TO DO: Don't show duplicate yellow sqares if letter is only in the word once
